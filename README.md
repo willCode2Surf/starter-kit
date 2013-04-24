7f1e59d3da53c71a02a021f5862091acc46bb11a
unknown <dwbrown@DT-DWBROWN3.ARM.DOMAIN>
2013-04-23 21:08:11 -0500

    mucho gracias

---
+++ README.md
@@ -0,0 +1,11 @@
+# EMBER4TWEEPS
+
+A fork and simple customization of basic ember starter kit with prob less then 100 lines of additional code, i dunno.
+
+## What's implemented so far?
+* rootElement override with tags added to application template (index.html)
+* query customization for twitter feed
+
+## Mixins
+* NONE at this time but cool mixins are a nice addition.
+
--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
-App = Ember.Application.create();
-
-App.Router.map(function() {
-  // put your routes here
+App = Em.Application.create({rootElement: 'tweets'});
+var debug = Em.debug;
+App.Router.map(function() {});
+App.listController = Ember.ArrayController.create({
+	searchTerm: 'emberjs'
+});
+$.getJSON('http://search.twitter.com/search.json?callback=?&q=' + App.listController.get('searchTerm'), function(response){
+	debug('http://search.twitter.com/search.json?callback=?&q=' + App.listController.get('searchTerm'));
+	App.listController.set('content', response.results);
+});
+App.IndexRoute = Ember.Route.extend();
+App.ResultView = Em.View.extend({
+	templateName: "blurb",
+	classNames: "whitelabel"
 });
 
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
-});
